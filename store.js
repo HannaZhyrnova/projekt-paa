3292a6fffdee34b1f7d4c7a9f9a9532da70cb765
annamriy1503 <hanna.zhyrnova@gmail.com>
2020-04-13 10:53:24 +0000

    Dodano możliwość zmiany statusu zadania

--- store.js
+++ store.js
@@ -1,12 +1,56 @@
+const updateTaskStatus = async (id, status) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(id),
+      status
+    }
+
+    service.mergeEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
+
+const createTask = async (title) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(uuid.v4()),
+      title,
+      status: 'open'
+    }
+
+    service.insertEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
+
+
+
+
+
+
+
+
+
+
+
+
 const listTasks = async () => (
   new Promise((resolve, reject) => {
     const query = new storage.TableQuery()
-      .select(['title'])
+      .select(['RowKey', 'title', 'status'])
       .where('PartitionKey eq ?', 'task')
 
     service.queryEntities(table, query, null, (error, result, response) => {
       !error ? resolve(result.entries.map((entry) => ({
-        title: entry.title._
+        id: entry.RowKey._,
+        title: entry.title._,
+        status: entry.status._
       }))) : reject()
     })
   })
@@ -31,20 +75,76 @@ const storage = require('azure-storage')
 const service = storage.createTableService()
 const table = 'tasks'
 const uuid = require('uuid')
-const createTask = async (title) => (
-  new Promise((resolve, reject) => {
-    const generator = storage.TableUtilities.entityGenerator
-    const task = {
-      PartitionKey: generator.String('task'),
-      RowKey: generator.String(uuid.v4()),
-      title
-    }
 
-    service.insertEntity(table, task, (error, result, response) => {
-      !error ? resolve() : reject()
-    })
-  })
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const init = async () => (
   new Promise((resolve, reject) => {
     service.createTableIfNotExists(table, (error, result, response) => {
@@ -56,5 +156,6 @@ const init = async () => (
 module.exports = {
   init,
 createTask,
-listTasks
+listTasks,
+updateTaskStatus
 }
